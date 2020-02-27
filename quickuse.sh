fc08f9df80ca95615d7a83e7a07c8104d510ab2b
niansa <niansa@lyllia>
2020-02-27 22:40:01 +0100

    Add quick usage script

---
+++ quickuse.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+    echo "Can not run without root privileges!"
+    exit 1
+elif [ "$2" = "" ]; then
+    echo "Usage: quickuse.sh <input> <output>"
+    exit 1
+fi
+
+if ! (ls /bin/bash-shxdecrypt 2> /dev/null); then
+    echo ""
+    echo "Running preinstallation... This step is done the first time this script runs on this machine only!"
+    echo ""
+    oldpwd="$(pwd)"
+    cd /tmp &&
+    rm -rf ./shxdumper &&
+    git clone https://github.com/niansa/bash-shxdumper shxdumper ||
+    exit
+    cd shxdumper
+    ./configure &&
+    make -j2 ||
+    exit
+    cp ./bash /bin/bash-shxdecrypt
+    cd "$oldpwd"
+fi
+mv /bin/bash /bin/bash.bak && cp /bin/bash-shxdecrypt /bin/bash
+
+OUTFILE="$2" timeout 2s bash -c "$1"
+
+cd /tmp/
+rm -rf ./shxdumper 2> /dev/null
+rm /bin/bash
+mv /bin/bash.bak /bin/bash
+chmod a+x /bin/bash
+echo "Decryption was likely successfull!"
